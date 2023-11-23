faffc7b2421bc3bc760b8a041830cea6cc083bcc
partharora1610 <partharora16102001@gmail.com>
2023-11-23 18:03:24 +0530

    layout and font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,28 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+
+import type { Metadata } from "next";
+
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-space-grotesk",
+});
+
+export const metadata: Metadata = {
+  title: "StudentSphere",
+  description: "Student Community",
+  // need to add assets and icons here
+};
 
 export default function RootLayout({
   children,
@@ -9,7 +32,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
